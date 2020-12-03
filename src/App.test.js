7998695ff71369aa9ce6ad6fd4cff3555a90a854
Bill Xiong <xiong.bill@yahoo.com>
2020-12-02 20:29:06 -0800

    MVP

---
+++ src/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import App from './App'
+import userEvent from '@testing-library/user-event';
+
+import { fetchShow as mockFetchShow } from './api/fetchShow'
+
+
+test('App fetches and renders data', async()=>{
+  // tell the test system what we want the data to return
+
+    const {getByText,getAllByText}=render(<App/>);
+
+    await waitFor(()=>{getByText(/select a season/i)}) ;
+
+    const button=getByText(/select a season/i)
+    userEvent.click(button);
+
+    const option= getByText(/Season 2/i);
+    userEvent.click(option);
+       expect(getAllByText(/episode/i)).toHaveLength(9)
+
+    }); 
